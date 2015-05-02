d56a592255988639da32befdf54849f176c58a2b
Irina Kalashnikova <IrinaKalashnikova@gmail.com>
2015-05-02 14:37:00 -0400

    set-up heroku

---
+++ login.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <title>Login</title>
+
+
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+
+    <link href="css/business-frontpage.css" rel="stylesheet">
+
+</head>
+
+<body>
+<script src="js/navbar.js"></script>
+    <div class="container">
+	<h2>Log in</h2>
+	<form>
+  <div class="form-group row">
+  <div class="col-md-4">
+    <label for="username">Username</label>
+    <input type="text" class="form-control" id="username" placeholder="Enter username">
+  </div>
+  </div>
+  
+  <div class="form-group row">
+  <div class="col-md-4">
+    <label for="password">Password</label>
+    <input type="password" class="form-control" id="password" placeholder="Password">
+  </div>
+  </div>
+	<input class="btn btn-default" type="submit" value="Log in">
+  </form>
+  </div>
+	
+	
+	
+	<!-- jQuery -->
+    <script src="js/jquery.js"></script>
+
+    <!-- Bootstrap Core JavaScript -->
+    <script src="js/bootstrap.min.js"></script>
+
+</body>
+
+</html>
---
+++ signup.php
@@ -0,0 +1,64 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>Sign up</title>
+
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+
+    <link href="css/business-frontpage.css" rel="stylesheet">
+
+</head>
+
+<body>
+<script src="js/navbar.js"></script>
+    <div class="container">
+	<h2>Sign up</h2>
+	<form>
+  <div class="form-group row">
+  <div class="col-md-4">
+    <label for="username">Username</label>
+    <input type="text" class="form-control" id="username" placeholder="Enter username">
+  </div>
+  </div>
+    <div class="form-group row">
+  <div class="col-md-4">
+    <label for="email">Email</label>
+    <input type="email" class="form-control" id="email" placeholder="Enter email address">
+  </div>
+  </div>
+  <div class="form-group row">
+  <div class="col-md-4">
+    <label for="password1">Password</label>
+    <input type="password" class="form-control" id="password1" placeholder="Password">
+  </div>
+  </div>
+  
+  <div class="form-group row">
+  <div class="col-md-4">
+    <input type="password" class="form-control" id="password2" placeholder="Re-enter password">
+  </div>
+  </div>
+	<input class="btn btn-default" type="submit" value="Sign up">
+  </form>
+  </div>
+  
+	
+	
+	
+	<!-- jQuery -->
+    <script src="js/jquery.js"></script>
+
+    <!-- Bootstrap Core JavaScript -->
+    <script src="js/bootstrap.min.js"></script>
+
+</body>
+
+</html>
